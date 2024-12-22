c0fa0e84803b5ba6bf7552668d2a3c6d8009072f
Alshanex <discordpermacc02@gmail.com>
2024-12-22 11:51:34 +0100

    Fallen brother spell finished including fame check and lang file

--- src/main/java/net/alshanex/equinox/spells/FallenBrothersSpell.java
+++ src/main/java/net/alshanex/equinox/spells/FallenBrothersSpell.java
@@ -5,17 +5,28 @@ import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.spells.*;
 import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.entity.mobs.SummonedSkeleton;
 import io.redspace.ironsspellbooks.entity.mobs.SummonedZombie;
+import io.redspace.ironsspellbooks.entity.spells.poison_cloud.PoisonSplash;
+import io.redspace.ironsspellbooks.particle.BlastwaveParticleOptions;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import io.redspace.ironsspellbooks.registries.SoundRegistry;
+import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.alshanex.equinox.EquinoxMod;
 import net.alshanex.equinox.entity.BloodBrotherEntity;
 import net.alshanex.equinox.entity.PoisonBrotherEntity;
+import net.alshanex.equinox.fame.FallenFameProvider;
+import net.alshanex.equinox.fame.UmbrakithFameProvider;
+import net.alshanex.equinox.item.ModItems;
+import net.alshanex.equinox.util.EUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
@@ -33,6 +44,7 @@ import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @AutoSpellConfig
 public class FallenBrothersSpell extends AbstractSpell {
@@ -84,6 +96,23 @@ public class FallenBrothersSpell extends AbstractSpell {
     }
 
     @Override
+    public boolean checkPreCastConditions(Level level, int spellLevel, LivingEntity entity, MagicData playerMagicData) {
+        AtomicInteger fallenFame = new AtomicInteger();
+        if(entity instanceof ServerPlayer player){
+            if(EUtils.hasItemInOrbSlot(player, ModItems.CORRUPTED_ORB.get())){
+                player.getCapability(FallenFameProvider.FALLEN_FAME).ifPresent(fame -> {
+                    fallenFame.set(fame.getFame());
+                });
+            }
+        }
+        if(fallenFame.get() >= 900){ return true;}
+        if(entity instanceof ServerPlayer player){
+            player.connection.send(new ClientboundSetActionBarTextPacket(Component.translatable("ui.equinox.cant_cast_spell_fame").withStyle(ChatFormatting.RED)));
+        }
+        return false;
+    }
+
+    @Override
     public void onCast(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
         int summonTime = 20 * 60;
         float radius = 1.5f + .185f * spellLevel;
@@ -96,12 +125,24 @@ public class FallenBrothersSpell extends AbstractSpell {
             bloodBrother.addEffect(new MobEffectInstance(MobEffectRegistry.RAISE_DEAD_TIMER.get(), summonTime, 0, false, false, false));
             var yrot = 6.281f / spellLevel * i + entity.getYRot() * Mth.DEG_TO_RAD;
             Vec3 spawn = Utils.moveToRelativeGroundLevel(world, entity.getEyePosition().add(new Vec3(radius * Mth.cos(yrot), 0, radius * Mth.sin(yrot))), 10);
+
             poisonBrother.setPos(spawn.x, spawn.y, spawn.z);
-            bloodBrother.setPos(spawn.x + 5, spawn.y, spawn.z + 5);
             poisonBrother.setYRot(entity.getYRot());
-            bloodBrother.setYRot(entity.getYRot());
             poisonBrother.setOldPosAndRot();
+            PoisonSplash poisonSplash = new PoisonSplash(world);
+            poisonSplash.setOwner(entity);
+            poisonSplash.moveTo(spawn.x, spawn.y, spawn.z);
+            poisonSplash.setDamage(0);
+            poisonSplash.setEffectDuration(40);
+
+            bloodBrother.setPos(spawn.x + 5, spawn.y, spawn.z + 5);
+            bloodBrother.setYRot(entity.getYRot());
             bloodBrother.setOldPosAndRot();
+            MagicManager.spawnParticles(world, ParticleHelper.BLOOD, bloodBrother.getX(), bloodBrother.getY() + .25f, bloodBrother.getZ(), 100, .03, .4, .03, .4, true);
+            MagicManager.spawnParticles(world, ParticleHelper.BLOOD, bloodBrother.getX(), bloodBrother.getY() + .25f, bloodBrother.getZ(), 100, .03, .4, .03, .4, false);
+            MagicManager.spawnParticles(world, new BlastwaveParticleOptions(SchoolRegistry.BLOOD.get().getTargetingColor(), 2), bloodBrother.getX(), bloodBrother.getBoundingBox().getCenter().y, bloodBrother.getZ(), 1, 0, 0, 0, 0, true);
+
+            world.addFreshEntity(poisonSplash);
             world.addFreshEntity(poisonBrother);
             world.addFreshEntity(bloodBrother);
         }
